be9d9abee0e75aa526defc1fc60f1103a0ca294f
adnan sherif <adnansherif@berkeley.edu>
2021-06-17 13:06:14 -0700

    sub

--- indexing.js
+++ indexing.js
@@ -2,20 +2,21 @@ window.addEventListener("load", initPopup);
 function initPopup(){
     chrome.storage.sync.get(["personal"], function (res) {   
         console.log(res);
+        var personInfo = res["personal"];
         var namediv = document.getElementById('fullname');
         var fullname = document.createElement('h6');
-        fullname.textContent = res["personal"]["first-name"] + " " + res["personal"]["last-name"] ;
+        fullname.textContent = personInfo["first-name"] + " " + personInfo["last-name"] ;
         fullname.style.textAlign = "center";
         namediv.appendChild(fullname);
 
         var cont = document.getElementById('cont');
-        var sample0 = {
-            "location": "Berkeley, CA",
-            "phone": "(415) 323-4000",
-            "email": "oski@berkeley.edu",
-            "site": "joesmith.site",
-            "github": "github.com/Tombarr"
-        };
+        // var sample0 = {
+        //     "location": "Berkeley, CA",
+        //     "phone": "(415) 323-4000",
+        //     "email": "oski@berkeley.edu",
+        //     "site": "joesmith.site",
+        //     "github": "github.com/Tombarr"
+        // };
         // var obj = JSON.parse(sample);
         //const contLi1 = document.createElement('li');
         const contLi2 = document.createElement('li');
@@ -52,8 +53,9 @@ function initPopup(){
         // contPara5.appendChild(conti5);
 
 
+        var personLocation = personInfo["address"] + ", " + personInfo["city"] + ", " + personInfo["state"];
         //conti1.textContent = sample4.firstname + " " + sample4.lastname;sample0.location + " | E: " + sample0.email + " | M: " + sample0.phone;
-        contPara2.textContent = sample0.location + " | E: " + sample0.email + " | M: " + sample0.phone;
+        contPara2.textContent = personLocation + " | E: " + personInfo["email"] + " | M: " + personInfo["phone"];
         contPara2.className = "sanserif";
         // contPara3.textContent = sample0.email;
         // contPara4.textContent = sample0.site;
@@ -75,29 +77,36 @@ function initPopup(){
     chrome.storage.sync.get(["education"], function (res) {  
         console.log(res);
         var educ = document.getElementById('educ');
-        var sample2 = {
-            "schools": [{
-                "degree": "M.S.",
-                "major": "Human Computer Interaction",
-                "school": "Massachusetts Institute of Technology",
-                "from": "09/2012",
-                "to": "08/2020",
-                "gpa": "4.00"
-            },
-                {
-                    "degree": "B.S.",
-                    "major": "Computer Science",
-                    "school": "Harvard University",
-                    "from": "09/2008",
-                    "to": "05/2012",
-                    "gpa": "3.91"
-                }]
-        };
+
+        // var sample2 = {
+        //     "schools": [{
+        //         "degree": "M.S.",
+        //         "major": "Human Computer Interaction",
+        //         "school": "Massachusetts Institute of Technology",
+        //         "from": "09/2012",
+        //         "to": "08/2020",
+        //         "gpa": "4.00"
+        //     },
+        //         {
+        //             "degree": "B.S.",
+        //             "major": "Computer Science",
+        //             "school": "Harvard University",
+        //             "from": "09/2008",
+        //             "to": "05/2012",
+        //             "gpa": "3.91"
+        //         }]
+        // };
+
+
+
     // var obj = JSON.parse(sample);
-    const schools  = sample2.schools;
-    for (let i = 0; i < schools.length; i++) {
+
+    var eduInfo = res["education"];
+    //const schools  = eduInfo["university"];
+    //////////Remove the hardcoding and adjust for multiple education fields////
+    for (let i = 0; i < 1; i++) {
         console.log(i);
-        const thisSkl = schools[i];
+        // const thisSkl = schools[i];
         const expLi = document.createElement('li');
         const expDiv1 = document.createElement('div');
         const expPara1 = document.createElement('p');
@@ -107,13 +116,17 @@ function initPopup(){
         const expSpan2 = document.createElement('span');
 
         expPara1.className = "sanserif";
-        expPara1.textContent = thisSkl.degree + ", " + thisSkl.major;
-        expTime.textContent = thisSkl.from + " – " + thisSkl.to;
+        // expPara1.textContent = thisSkl.degree + ", " + thisSkl.major;
+        // expTime.textContent = thisSkl.from + " – " + thisSkl.to;
+        expPara1.textContent = eduInfo["state"] + ", " + eduInfo["field"];
+        expTime.textContent = eduInfo["from"] + " – " + eduInfo["to"];
         expDiv1.appendChild(expPara1);
         expDiv1.appendChild(expTime);
 
-        expSpan1.textContent = thisSkl.school;
-        expSpan2.textContent = "GPA: " + thisSkl.gpa;
+        // expSpan1.textContent = thisSkl.school;
+        // expSpan2.textContent = "GPA: " + thisSkl.gpa;
+        expSpan1.textContent = eduInfo["university"];
+        expSpan2.textContent = "GPA: " + eduInfo["gpa"];
         expDiv2.appendChild(expSpan1);
         expDiv2.appendChild(expSpan2);
 
@@ -122,10 +135,16 @@ function initPopup(){
         educ.appendChild(expLi);
     }
     }); 
+
+
+    // Still Hard Coded
     var sample3 = {
     "technologies": ["JavaScript", "PHP", "HTML5", "CSS3", "Bootstrap", "React"],
     "courses": ["Responsive Design", "A/B Testing", "Mobile Development", "Data Visualization", "Usability Testing"]
     };
+
+
+
     //COURSES
     var courses = document.getElementById('relevant-courses');
     // var obj = JSON.parse(sample);
@@ -219,9 +238,10 @@ function initPopup(){
                 }]
             }
         };
+
+
         function createExps(exps, positions) {
-            for (let i = 0; i < positions.length; i++) {
-                console.log(i);
+            for (let i = 1; i < Object.keys(positions).length+1-2; i++) {
                 const thisPos = positions[i];
                 const expLi = document.createElement('li');
                 const expHeader = document.createElement('header');
@@ -236,10 +256,12 @@ function initPopup(){
                 expHeader.appendChild(expPara1);
                 expHeader.appendChild(expTime);
 
-                expSpan.textContent = thisPos.position;
+                expSpan.textContent = thisPos["job-title"];
 
-                const desc = thisPos.description;
+                const desc = thisPos["role-description"].split("\n");
                 for (let j = 0; j < desc.length; j++) {
+                    //console.log(desc[j].trim().length);
+                    if (desc[j].trim().length == 0) continue;
                     const expDescLi = document.createElement('li');
                     expDescLi.textContent = desc[j];
                     expUl.appendChild(expDescLi);
@@ -251,32 +273,97 @@ function initPopup(){
                 exps.appendChild(expLi);
             }
         }
-        function createCategory(num,category) {
+        function createCategory(num,categoryName) {
             var categories = document.getElementById('categories');
             var line = document.createElement('hr');
             var heading = document.createElement('h6');
             var list = document.createElement('ol');
             list.id = "cat" + num;
-            heading.textContent = category.name;
+            heading.textContent = categoryName;
             categories.appendChild(line);
             categories.appendChild(heading);
             categories.appendChild(list);
         }
         var exps = document.getElementById('cat1');
-        // var obj = JSON.parse(sample);
-        var positions = sample.category1.items;
+        // make the first category also dynamic
+        $($('#categories').find('h6')[0]).text(res['categories']['names'][1]);
+        var positions = res['categories']['category1'];
         createExps(exps,positions);
-        if(sample.category2) {
-            createCategory(2,sample.category2)
+        if(Object.keys(res['categories']['category2']).length > 2) {
+            createCategory(2,res['categories']['names'][2]);
             var exps = document.getElementById('cat2');
-            var positions = sample.category2.items;
+            var positions = res['categories']['category2'];
             createExps(exps,positions);
         }
-        if(sample.category3) {
-            createCategory(3,sample.category3)
+        console.log(Object.keys(res['categories']['category3']).length);
+        if(Object.keys(res['categories']['category3']).length > 2) {
+            createCategory(3,res['categories']['names'][3]);
             var exps = document.getElementById('cat3');
-            var positions = sample.category3.items;
+            var positions = res['categories']['category3'];
             createExps(exps,positions);
         }  
+        var sheet = $('.sheet')[0];
+        console.log(sheet);
+        console.log(""+sheet.clientHeight+"and"+ sheet.scrollHeight);
     });
+    
 }
+
+// personal:
+// address: "2550 telegraph avenue"
+// age-group: "29"
+// city: "Berkeley"
+// country: "united states"
+// current-company: "94564"
+// current-position: "94564"
+// disability-status: "yes"
+// email: "adnansherif@berkeley.edu"
+// ethnicity: "asian"
+// first-name: "adnan"
+// gender: "man"
+// github: "adnansherif@berkeley.edu"
+// last-name: "Sherif"
+// legal-status: "yes"
+// linkedin: "adnansherif@berkeley.edu"
+// phone: "4243108159"
+// pronouns: "he/him"
+// state: "AL"
+// veteran-status: "yes"
+// working-visa: "no"
+// zip-code: "94564"
+
+// education:
+// field: "compiuter science "
+// from: "2018"
+// gpa: "4.0"
+// state: "BS"
+// to: "2021"
+// university: "Berkeley"
+// __proto__: Object
+// __proto__: Object
+
+
+// Object
+// categories:
+// category1:
+// 1:
+// addVbutton: ""
+// company: "Experian"
+// from: "2021-05"
+// job-title: "Machine learning internship"
+// location: "Bekreley"
+// role-description: "•Developed convenient, cost-effective, and privacy first proctoring platform for online University exams. The many to one proctoring platform has unique features including one to one audio, visual, and chat communication.\n\n•Implemented webRTC peer connections relayed through Kurento media servers and  hosted on AWS EC2 instances for audio-visual communication. \n\n•Built course management system and proctoring UI with Angular framework.Used react native for mobile app\n•Build rest api for creating sessions, authentication, and recording ops with spring java and mySQL."
+// role-version0: "•Developed convenient, cost-effective, and privacy first proctoring platform for online University exams. The many to one proctoring platform has unique features including one to one audio, visual, and chat communication.\n\n•Implemented webRTC peer connections relayed through Kurento media servers and  hosted on AWS EC2 instances for audio-visual communication. \n\n•Built course management system and proctoring UI with Angular framework.Used react native for mobile app\n\n•Build rest api for creating sessions, authentication, and recording ops with spring java and mySQL."
+// sel: "on"
+// selected0: true
+// to: "2021-12"
+// versionnum: 1
+// __proto__: Object
+// 2: {addVbutton: "", company: "Pinxuan", from: "2021-01", job-title: "Data scientist", location: "Berkeley", …}
+// num: 2
+// sel: []
+// __proto__: Object
+// category2: {1: {…}, 2: {…}, num: 2, sel: Array(0)}
+// category3: {num: 0, sel: Array(0)}
+// __proto__: Object
+// __proto__: Object
